11d69726e5617c8edab0985fa3f2b17fabef3331
abduyus <35887528+abduyus@users.noreply.github.com>
2024-01-04 16:08:23 +0000

    Adding Kayyan account

--- script.js
+++ script.js
@@ -91,6 +91,27 @@ const account4 = {
   locale: 'en-GB',
 };
 
+const account5 = {
+  owner: 'Kayyan Nawaz',
+  movements: [1030, -90, 1000, 700, 50, 90, -19],
+  interestRate: 8,
+  pin: 5,
+
+  movementsDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+
+  currency: 'GBP',
+  locale: 'en-GB',
+};
+
 const accounts = [account1, account2, account3, account4];
 
 /////////////////////////////////////////////////
